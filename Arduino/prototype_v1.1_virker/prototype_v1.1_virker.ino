1c0e00813fce47f5ff1deeef6c906bb26477a433
Luca Casa <lc301092@gmail.com>
2018-05-31 17:51:14 +0200

    DENNE SKAL BRUGES TIL BILAG
    bilag

--- Arduino/prototype_v1.1_virker/prototype_v1.1_virker.ino
+++ Arduino/prototype_v1.1_virker/prototype_v1.1_virker.ino
@@ -3,17 +3,16 @@
 #define NUMPIXELS      3 // number of neopixels in strip
 
 int indexPin = 3;
-int middlePin = 6;
-int ringPin = 9;
-int pinkyPin = 12;
 
 // Adafruit class for a neopixles
 Adafruit_NeoPixel index = Adafruit_NeoPixel(NUMPIXELS, indexPin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel middle = Adafruit_NeoPixel(NUMPIXELS, middlePin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel ring = Adafruit_NeoPixel(NUMPIXELS, ringPin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pinky = Adafruit_NeoPixel(NUMPIXELS, pinkyPin, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel hand[] = {index, middle, ring, pinky};
+index.begin();
+
+index.setPixelColor(2, 255, 255, 255);
+// this is neopixle syntax
+index.show();
+
 
 // -----------------------------
 
@@ -33,7 +32,7 @@ bool finger4 = 0;
 
 int fingers[] = {finger0, finger1, finger2, finger3, finger4};
 
-int test[4][3] = {
+int fingerBaseColor[4][3] = {
   {255, 0, 0},
   {127, 127, 0},
   {0, 0, 255},
@@ -58,7 +57,7 @@ int thresholdArray[] = {
 int threshold = 1015;
 //-----------------------------
 
-// longs are used to make sure there is no overflow 
+// longs are used to make sure there is no overflow
 long redColor;
 long greenColor;
 long blueColor;
@@ -71,15 +70,15 @@ void setup() {
   pinky.begin();
 
   for (int i = 0; i < NUM_FINGERS;  i++) {
-    // following comment can change the threshold dynamically to fit the environment 
+    // following comment can change the threshold dynamically to fit the environment
     // thresholdArray[i] = analogRead(i);
     thresholdArray[i] = threshold;
-    if(i == 0){
+    if (i == 0) {
       // the thumb needs a lower threshold
-    thresholdArray[i] -= 15;  
-      }
+      thresholdArray[i] -= 15;
+    }
   }
-  
+
 }
 
 void loop() {
@@ -103,39 +102,40 @@ void loop() {
     }
   }
 
-// if the thumb is strechted 
+  // if the thumb is strechted
   if (!fingers[0]) {
     // loop starts at 1 because of the thumb
     for (int i = 1; i < NUM_FINGERS; i++) {
       if (fingers[i]) {
         // subtracting i with 1 to make sure the it corresponds to the colors from the 2d array
-        redColor += test[i - 1][0];
-        greenColor += test[i - 1][1];
-        blueColor += test[i - 1][2];
+        redColor += fingerBaseColor[i - 1][0];
+        greenColor += fingerBaseColor[i - 1][1];
+        blueColor += fingerBaseColor[i - 1][2];
       }
     }
   }
-  
+
+  redColor = 1000 * (255.0 / 3000)
   for (int i = 1; i < NUM_FINGERS ; i++) {
- // check if the thumb is in the palm     
-      if (fingers[0] ) {
-        redColor = test[i - 1][0];
-        greenColor = test[i - 1][1];
-        blueColor = test[i - 1][2];
-      }
-      sumMix = redColor + blueColor + greenColor;
+    // check if the thumb is in the palm
+    if (fingers[0] ) {
+      redColor = fingerBaseColor[i - 1][0];
+      greenColor = fingerBaseColor[i - 1][1];
+      blueColor = fingerBaseColor[i - 1][2];
+    }
+    sumMix = redColor + blueColor + greenColor;
 
-      // the colors get normalized if there is more than 1 active finger
-      // sumMix increases af more colors are selected
-      redColor = redColor * (255.0 / sumMix);
-      greenColor = greenColor * (255.0 / sumMix);
-      blueColor = blueColor * (255.0 / sumMix);
+    // the colors get normalized if there is more than 1 active finger
+    // sumMix increases af more colors are selected
+    redColor = redColor * (255.0 / sumMix);
+    greenColor = greenColor * (255.0 / sumMix);
+    blueColor = blueColor * (255.0 / sumMix);
 
-      for (int j = 0; j < NUMPIXELS; j ++) {
-        // for each pixle on each finger, give that a color
-        hand[i - 1].setPixelColor(j, redColor*fingers[i], greenColor*fingers[i], blueColor*fingers[i]);
-        // this is neopixle syntax
-        hand[i - 1].show();
+    for (int j = 0; j < NUMPIXELS; j ++) {
+      // for each pixle on each finger, give that a color
+      hand[i - 1].setPixelColor(j, redColor * fingers[i], greenColor * fingers[i], blueColor * fingers[i]);
+      // this is neopixle syntax
+      hand[i - 1].show();
 
     }
   }
